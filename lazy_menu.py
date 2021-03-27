83073005805da32df191026480c8c93892794dc9
AmosNimos <amounimous@protonmail.com>
2021-03-27 13:00:02 -0400

    Update

---
+++ lazy_menu.py
@@ -0,0 +1,21 @@
+import dmenu
+import os
+path = "/home/plato/Documents"
+file = "p"
+results = []
+options = []
+output=""
+with open(path+"/"+file, 'r') as p:
+	for line in p:
+		if line[0] != "#":
+			lines = str(line).split(" !|! ")
+			options.append(str(lines[0]))
+			results.append(str(lines[1])[:-1])
+entry = dmenu.show(options,prompt="Service:")
+for index in range(len(options)):
+	if str(entry) == str(options[index]):
+		selected = index
+output = results[selected]
+os.system("sleep 0.5")
+os.system("xdotool type --clearmodifiers "+output)
+os.system("xdotool key KP_Enter")
