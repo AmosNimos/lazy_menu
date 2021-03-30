5d55da91c6074afbc47f697278ce38035cf6d1f0
Amos Nimos <76786560+AmosNimos@users.noreply.github.com>
2021-03-30 15:24:14 -0400

    Update lazy_menu.py

--- lazy_menu.py
+++ lazy_menu.py
@@ -1,23 +1,28 @@
+#Fill the output of the selected input
+#Warning! Do not use & symbol in your output!
+
 import dmenu
 import os
-#replace with your own username
-home_dir="/home/user"
-path = home_dir+"/Documents"
+path = "/home/plato/Documents"
 file = "p"
 results = []
 options = []
 output=""
 with open(path+"/"+file, 'r') as p:
 	for line in p:
-		if line[0] != "#":
-			lines = str(line).split(" !|! ")
+		if line[0] != "#" and line != "\n":
+			lines = str(line).split(" & ")
 			options.append(str(lines[0]))
 			results.append(str(lines[1])[:-1])
 entry = dmenu.show(options,prompt="Service:")
 for index in range(len(options)):
 	if str(entry) == str(options[index]):
 		selected = index
-output = results[selected]
+output = str(results[selected])
+if "&" in output:
+	print("output "+str(lines[1])+" contain (&) symbol, please remove it and try again.")
+	exit()
+#print(output)
 os.system("sleep 0.5")
-os.system("xdotool type --clearmodifiers "+output)
+os.system("xdotool type --clearmodifiers "+str(output))
 os.system("xdotool key KP_Enter")
